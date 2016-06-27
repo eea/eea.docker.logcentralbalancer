09e3c113f4b440c0404047779b7121af2bb7f3fd
lucas <luca.pisani@abstract.it>
2016-06-27 12:52:17 +0200

    refs #73416 add balancer for 1514 udp

--- docker-start.sh
+++ docker-start.sh
@@ -4,11 +4,11 @@ cp /tmp/nginx.conf.template /etc/nginx/nginx.conf
 
 GRAYLOG_HOSTS=${GRAYLOG_HOSTS:=graylog-master,graylog-client-1}
 
-GRAYLOGSERVERSUDP=""
-GRAYLOGSERVERSTCP=""
+GRAYLOGSERVERSGELF=""
+GRAYLOGSERVERSSYSLOG=""
 
 for service in $(echo $GRAYLOG_HOSTS | sed 's/ //g' | sed 's/,/\n/g'); do
-    GRAYLOGSERVERSUDP=$GRAYLOGSERVERSUDP"\tserver $service:12201;\n"
-    GRAYLOGSERVERSTCP=$GRAYLOGSERVERSTCP"\tserver $service:1514;\n"
+    GRAYLOGSERVERSGELF=$GRAYLOGSERVERSGELF"\tserver $service:12201;\n"
+    GRAYLOGSERVERSSYSLOG=$GRAYLOGSERVERSSYSLOG"\tserver $service:1514;\n"
 done
-sed -i 's/GRAYLOGSERVERSUDP/'"$GRAYLOGSERVERSUDP"'/g;s/GRAYLOGSERVERSTCP/'"$GRAYLOGSERVERSTCP"'/g' /etc/nginx/nginx.conf
+sed -i 's/GRAYLOGSERVERSGELF/'"$GRAYLOGSERVERSGELF"'/g;s/GRAYLOGSERVERSSYSLOG/'"$GRAYLOGSERVERSSYSLOG"'/g' /etc/nginx/nginx.conf
