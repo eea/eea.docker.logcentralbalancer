4e386b2902a3172a061644b04d3278b90f1320d4
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2019-04-15 19:34:42 +0300

    fix variable

--- docker-start.sh
+++ docker-start.sh
@@ -7,7 +7,7 @@ GRAYLOG_HOST=${GRAYLOG_HOST:-"graylog-client"}
 RESOLVER_IP=${RESOLVER_IP:-"169.254.169.250"}
 RESOLVER_TIMEOUT=${RESOLVER_TIMEOUT:-"15s"}
 
-sed -i "s/GRAYLOG/$GRAYLOG_HOSTS/g" /etc/nginx/nginx.conf
+sed -i "s/GRAYLOG/$GRAYLOG_HOST/g" /etc/nginx/nginx.conf
 sed -i "s/RESOLVER_IP/$RESOLVER_IP/g" /etc/nginx/nginx.conf
 sed -i "s/RESOLVER_TIMEOUT/$RESOLVER_TIMEOUT/g" /etc/nginx/nginx.conf
 
